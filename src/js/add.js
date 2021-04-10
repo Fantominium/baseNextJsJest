e9934f103fcef26e778844bfe73271bbbd66d9e9
malcolm <Malcom@Malcolms-MacBook-Pro.local>
2021-04-10 18:29:30 +0100

    integration test tut

--- src/js/add.js
+++ src/js/add.js
@@ -1,3 +1,7 @@
 export default function add(x, y) {
     return x + y
 }
+
+export function total(t, o) {
+    return "$" + add(t, o) 
+}
--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,5 +1,5 @@
-import add from '../js/add'
+import add, {total}from '../js/add'
 
 test('should test the add function ', () => {
-    expect(add(1,2)).toBe(3)
+    expect(total(1,2)).toBe('$3')
 })
